f5cf6a9ed173dc1edfdd7c6e1a800149554346ef
ErickIgles <erick.almeida2125@gmail.com>
2025-05-27 10:58:53 -0300

    feat: adicionando model Task ao admin

--- Task/admin.py
+++ Task/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'status', 'created', 'modified', 'active')
+    list_filter = ('title', 'description')
 
