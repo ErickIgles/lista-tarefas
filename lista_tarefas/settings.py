089e419e518463229032f7c1f6de28b7b9847165
ErickIgles <erick.almeida2125@gmail.com>
2025-05-27 10:28:36 -0300

    feat: adicionando app Task no INSTALLED_APPS

--- lista_tarefas/settings.py
+++ lista_tarefas/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_filters',
+    'rest_framework',
+
+    'Task.apps.TaskConfig',
 ]
 
 MIDDLEWARE = [
