6cf41fc548de0d2783708b5f034c1a23ac47f819
ErickIgles <erick.almeida2125@gmail.com>
2025-05-28 17:43:36 -0300

    chore: adiciona configurações de arquivos estáticos e de mídia

--- lista_tarefas/settings.py
+++ lista_tarefas/settings.py
@@ -124,7 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    Path(BASE_DIR, 'lista_tarefas/static')
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
