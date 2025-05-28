bb7583b0b70f3b4e0f477645b4277de818027ea5
ErickIgles <erick.almeida2125@gmail.com>
2025-05-28 17:38:26 -0300

    feat: adicionado IndexTemplateView para renderizar index.html

--- Task/views.py
+++ Task/views.py
@@ -2,10 +2,15 @@ from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializers
 from rest_framework.permissions import IsAuthenticated
+from django.views.generic import TemplateView
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializers
     permission_classes = [IsAuthenticated]
+
+
+class IndexTemplateView(TemplateView):
+    template_name = 'index.html'
 
