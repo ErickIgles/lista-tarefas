9fa2087fda93555ff8f7ad834915f4dc9575a052
ErickIgles <erick.almeida2125@gmail.com>
2025-06-02 16:33:25 -0300

    refactor: alterado permissão de acesso na TaskViewSet

--- Task/views.py
+++ Task/views.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializers
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializers
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
